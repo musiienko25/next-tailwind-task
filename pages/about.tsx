99f7021915caf939750c99f9f584570bb1122ad7
musiienko25 <devmusiienko@gmail.com>
2024-02-12 20:41:00 +0200

    deleted minor moments

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,10 +5,7 @@ const About: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold mb-6">About Us</h1>
-      <p className="text-lg">
-        Welcome to our store! We are committed to providing quality products and
-        excellent customer service.
-      </p>
+      <p className="text-lg">Welcome to About page!</p>
       <div className="mt-4">
         <Link className="text-blue-500 hover:text-blue-700" href="/" passHref>
           Go back home
